cd3ef06844773524a97f6723463eed0ef46f7964
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2020-07-02 08:04:38 +0530

    Create IsSubsequence.py

---
+++ Two-Pointer/IsSubsequence.py
@@ -0,0 +1,20 @@
+"""
+Given a string s and a string t, check if s is subsequence of t.
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+"""
+
+#CODE:
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if s and not t:
+            return False
+        if not s and t:
+            return True
+        
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+            
+        return i == len(s)
