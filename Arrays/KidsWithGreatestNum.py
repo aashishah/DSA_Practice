e4256477d1dda49adbad90520fcf50d2f909cbd2
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2020-09-23 12:34:19 +0530

    Create KidsWithGreatestNum.py

---
+++ Arrays/KidsWithGreatestNum.py
@@ -0,0 +1,23 @@
+"""QUESTION:
+Given the array candies and the integer extraCandies, where candies[i] represents the number of candies that the ith kid has.
+For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. 
+Notice that multiple kids can have the greatest number of candies.
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true]
+"""
+
+#CODE:
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxnum = max(candies)
+        ans = []
+        for i in candies:
+            if i + extraCandies >= maxnum:
+                ans.append(True)
+            else:
+                ans.append(False)
+                
+        return ans
+
+ 
