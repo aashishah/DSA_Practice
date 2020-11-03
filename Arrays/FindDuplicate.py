4b1895bc98f3988d4486485dc8b98c21a292c8dc
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2020-11-03 12:48:18 +0530

    Create FindDuplicate.py

---
+++ Arrays/FindDuplicate.py
@@ -0,0 +1,23 @@
+#QUESTION:
+#Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+#There is only one duplicate number in nums, return this duplicate number.
+
+#Naive solution, sort so that duplicates are adjacent. T + S: O(nlogn) + O(1) or O(n)
+#CODE:
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return nums[i]
+                
+#Better Solution: Create a set and return number that repeats. T + S: O(n) + O(n)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        visited = set()
+        for n in nums:
+            if n in visited:
+                return n
+            visited.add(n)
+            
+#Best: T + S: O(n) + O(1) Cycle Detection
