b11e6320606192b7bce0caac1e9fa02014c68670
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2020-11-03 12:23:33 +0530

    Update SingleNumber.py

--- Bit Manipulation/SingleNumber.py
+++ Bit Manipulation/SingleNumber.py
@@ -18,7 +18,7 @@ class Solution:
                 return n
                 
 #Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
-#We can XOR all bits together to find the unique number in constant time and space.
+#We can XOR all bits together to find the unique number in linear time and constant space.
 #CODE:
 class Solution(object):
     def singleNumber(self, nums):
