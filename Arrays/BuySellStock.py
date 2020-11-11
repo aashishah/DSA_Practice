ac01f1f1c3bc4f8b8a9ae81485797280ae31feca
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2020-11-11 19:05:10 +0530

    Update BuySellStock.py

--- Arrays/BuySellStock.py
+++ Arrays/BuySellStock.py
@@ -23,3 +23,15 @@ class Solution:
                 maxprofit = prices[i] - minprice
                 
         return maxprofit
+
+#ELEGANT CODE:
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minprice = sys.maxsize
+        maxprofit = 0
+        for p in prices:
+            minprice = min(p, minprice)
+            maxprofit = max(maxprofit, price - minprice)
+                
+        return maxprofit
+
