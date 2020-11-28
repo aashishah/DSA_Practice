538957e473e23c472c0036181a83d72aa2951e90
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2020-11-28 22:27:39 +0530

    Update GroupAnagrams.py

--- Strings/GroupAnagrams.py
+++ Strings/GroupAnagrams.py
@@ -13,5 +13,45 @@ class Solution:
             ans[tuple(sorted(s))].append(s)
         return ans.values()
  
- 
+#DIFF APPROACH: Using unique prime multiplication products.-> Much like hash mapping. -> Might cause overflow in C/C++ 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        primes = {'a': 2, 
+                  'b': 3, 
+                  'c': 5, 
+                  'd': 7, 
+                  'e': 11, 
+                  'f': 13,
+                  'g': 17,
+                  'h': 19,
+                  'i': 23,
+                  'j': 29,
+                  'k': 31,
+                  'l': 37,
+                  'm': 41,
+                  'n': 43,
+                  'o': 47,
+                  'p': 53,
+                  'q': 59,
+                  'r': 61,
+                  's': 67, 
+                  't': 71,
+                  'u': 73,
+                  'v': 79,
+                  'w': 83,
+                  'x': 89,
+                  'y': 97,
+                  'z': 101
+                 }
+        res = collections.defaultdict(list)
+        for s in strs:
+            product = 1
+            for c in s:
+                product *= primes[c]
+            if product in res:
+                res[product].append(s)
+            else:
+                res[product] = [s]
+                
+        return res.values()
  
