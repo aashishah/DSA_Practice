81e38f79f32ffe582986712ca12b7e876c7efb83
Aashi Shah <49470807+aashishah@users.noreply.github.com>
2021-04-23 18:23:27 +0530

    Update NextPermutation.py

--- Arrays/NextPermutation.py
+++ Arrays/NextPermutation.py
@@ -1,4 +1,5 @@
 #EXPLANATION:
+'''
 Lexicographically: In dictionary order
 The O(N) inplace algorithm is not easy. It is indeed tricky. The editorial has a nice explanation along with a nice animation.
 Use the example: [1,5,8,4,7,6,5,3,1]
@@ -6,6 +7,7 @@ Start from the end and scan leftwards till you find the first number which break
 Now if we swap 4 and 7, we will definitely get a larger number than the current one. But that is not the next smallest number.
 We want to replace 4 with a digit which is just larger than 4 - smallest number larger than 4 on right. That is 5. We swap the two and get: [1,5,8,5,7,6,4,3,1]
 Now [1,5,8,5,7,6,4,3,1] > [1,5,8,4,7,6,5,3,1], but not the smallest number larger than it. If we reverse all digits after 5, we will get the first number in that sequence. [1,5,8,5,1,3,4,6,7]
+'''
 
 #CODE:
 class Solution:
